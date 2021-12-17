3da04897c1e363b8d6dde640bd252109b45b3151
Rossel <rossel@theshiftleft.com>
2021-12-18 01:33:37 +0800

    fixed bridge link

--- src/config/constants/bridgeNetworks.ts
+++ src/config/constants/bridgeNetworks.ts
@@ -7,14 +7,14 @@ const bridgeNetworks = [
       'chainId': 56,
       'chainCurrency': 'BNB',
       'tokens': bridgeTokens[Network.BSC],
-      'redirect_url': '/bridge/?outputChainId=250' // might remove later on if we add more chains
+      'redirect_url': 'https://bsc.hyperjump.app/bridge/?outputChainId=250' // might remove later on if we add more chains
     },
     {
       'name': 'Fantom',
       'chainId': 250,
       'chainCurrency': 'FTM',
       'tokens': bridgeTokens[Network.FTM],
-      'redirect_url': '/bridge/?outputChainId=56'
+      'redirect_url': 'https://ftm.hyperjump.app/bridge/?outputChainId=56'
     }
 ]
 
