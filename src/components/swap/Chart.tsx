a1cc2ec952ad6932b037f36fed8b794d35f7fe4c
Edgar Alan Balderama <eabalderama@gmail.com>
2022-02-02 13:25:26 +0800

    Few chart css changes

--- src/components/swap/Chart.tsx
+++ src/components/swap/Chart.tsx
@@ -4,25 +4,21 @@ import styled from 'styled-components'
 
 const ChartFrame = styled(Iframe)`
     position: relative;
+    width:100%;
     height: 90vh;
     top: 0;
     left: 0;
     border: 0;
     flex: 2;
     display: none !important;
-    ${({ theme }) => theme.mediaQueries.xs} {
-    }
-
-    ${({ theme }) => theme.mediaQueries.sm} {
-    }
-    
+    min-width: 995px;
     ${({ theme }) => theme.mediaQueries.lg} {
         display: block !important;
     }
 `
 export default function Chart({tokenPair, network}) {
     const networkName = network === 'FTM' ? 'fantom' : 'bsc'
-    const url = tokenPair !== undefined ? `https://dexscreener.com/${networkName}/${tokenPair}?embed=1&theme=dark` : `https://dexscreener.com/${networkName}/0x?embed=1&theme=dark`
+    const url = tokenPair !== undefined ? `https://dexscreener.com/${networkName}/${tokenPair}?embed=0&theme=dark&info=0&trades=0` : `https://dexscreener.com/${networkName}/0x?embed=1&theme=dark&info=0&trades=0`
     return(
         <ChartFrame 
             url={url}
