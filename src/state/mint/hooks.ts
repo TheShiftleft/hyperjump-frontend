7da50d4990c09df5fd92e402a9de45874f3e2af0
Edgar Alan Balderama <eabalderama@gmail.com>
2022-03-17 20:24:45 +0800

    Fix add liquidity console error

--- src/state/mint/hooks.ts
+++ src/state/mint/hooks.ts
@@ -137,11 +137,11 @@ export function useDerivedMintInfo(
   }
 
   if (pairState === PairState.INVALID) {
-    error = error ?? TranslateString(136, 'Invalid pair')
+    error = error ?? 'Invalid pair'
   }
 
   if (!parsedAmounts[Field.CURRENCY_A] || !parsedAmounts[Field.CURRENCY_B]) {
-    error = error ?? TranslateString(84, 'Enter an amount')
+    error = error ?? 'Enter an amount'
   }
 
   const { [Field.CURRENCY_A]: currencyAAmount, [Field.CURRENCY_B]: currencyBAmount } = parsedAmounts
