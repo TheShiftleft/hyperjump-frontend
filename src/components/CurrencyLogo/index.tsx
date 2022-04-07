5e930432e53ca9a21fac2d4d3086cd46fb0cfdb1
Edgar Alan Balderama <eabalderama@gmail.com>
2022-04-07 11:19:07 +0800

    Fix currency logo use token icons for native tokens instead of network icons

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -9,9 +9,6 @@ import { filterTokens } from '../../views/Farms/components/filtering'
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
-import BSCIcon from '../../uikit/widgets/Menu/icons/BSCNetwork'
-import BSCTestnetIcon from '../../uikit/widgets/Menu/icons/BSCNetworkTestnet'
-import FTMIcon from '../../uikit/widgets/Menu/icons/FTMNetwork'
 
 // FIXME replace this
 const getTokenLogoURL = (address: string) => `https://tokens.hyperjump.app/images/${address}.png`
@@ -20,11 +17,6 @@ const StyledLogo = styled(Logo)<{ size: string }>`
   width: ${({ size }) => size};
   height: ${({ size }) => size};
 `
-const networkIcon: Record<Network, any> = {
-  [Network.BSC]: BSCIcon,
-  [Network.BSC_TESTNET]: BSCTestnetIcon,
-  [Network.FANTOM]: FTMIcon
-}
 
 export default function CurrencyLogo({
   currency,
@@ -43,9 +35,13 @@ export default function CurrencyLogo({
 
   const defaultTokenIcon = config.networkToken?.symbol.toLowerCase() === 'bnb' ? '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c' : config.networkToken?.symbol.toLowerCase() === 'ftm' ? '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83' : 'token'
   const netWorkTokenIconImg = searchTokenIconName[0]?.address === undefined ? defaultTokenIcon : searchTokenIconName[0]?.address
-  const Icon = networkIcon[config.network]
   const srcs: string[] = useMemo(() => {
-    if (currency === config.baseCurrency) return []
+    if (currency === config.baseCurrency) {
+      return [
+        `/images/tokens/${netWorkTokenIconImg}.png`,
+        getTokenLogoURL(netWorkTokenIconImg)
+      ]
+    }
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
         return [
@@ -59,13 +55,7 @@ export default function CurrencyLogo({
         getTokenLogoURL(currency.address)]
     }
     return []
-  }, [config.baseCurrency, currency, uriLocations])
-  
-  if (currency === config.baseCurrency) {
-    return (
-      <Icon style={{...style, borderRadius: '24px', width: '24px', height: '24px'}} alt={`${currency?.symbol ?? 'token'} logo`}/>
-    )
-  }
+  }, [config.baseCurrency, currency, uriLocations, netWorkTokenIconImg])
 
   return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />
 }
