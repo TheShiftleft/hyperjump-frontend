efc65eb6ef4198181f1519df7033e49a86dbe2a2
Bryann Revina <bryann@theshiftleft.com>
2022-04-12 12:08:52 +0800

    Update files (From Angry-Mech)

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -1,20 +1,17 @@
-import { Currency, Token } from '@hyperjump-defi/sdk'
+import { Currency, Network, Token } from '@hyperjump-defi/sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import getNetwork from 'utils/getNetwork'
+
+import { useAllTokens } from 'hooks/Tokens'
+import { filterTokens } from '../../views/Farms/components/filtering'
+
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
 // FIXME replace this
 const getTokenLogoURL = (address: string) => `https://tokens.hyperjump.app/images/${address}.png`
-
-const StyledBnbLogo = styled.img<{ size: string }>`
-  width: ${({ size }) => size};
-  height: ${({ size }) => size};
-  box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.075);
-  border-radius: 24px;
-`
 
 const StyledLogo = styled(Logo)<{ size: string }>`
   width: ${({ size }) => size};
@@ -33,31 +30,32 @@ export default function CurrencyLogo({
 }) {
   const uriLocations = useHttpLocations(currency instanceof WrappedTokenInfo ? currency.logoURI : undefined)
   const { config } = getNetwork()
-  const srcs: string[] = useMemo(() => {
-    if (currency === config.baseCurrency) return []
+  const allTokens = useAllTokens()
+  const searchTokenIconName = filterTokens(Object.values(allTokens), config.networkToken?.symbol.toLowerCase())
 
+  const defaultTokenIcon = config.networkToken?.symbol.toLowerCase() === 'bnb' ? '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c' : config.networkToken?.symbol.toLowerCase() === 'ftm' ? '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83' : 'token'
+  const netWorkTokenIconImg = searchTokenIconName[0]?.address === undefined ? defaultTokenIcon : searchTokenIconName[0]?.address
+  const srcs: string[] = useMemo(() => {
+    if (currency === config.baseCurrency) {
+      return [
+        `/images/tokens/${netWorkTokenIconImg}.png`,
+        getTokenLogoURL(netWorkTokenIconImg)
+      ]
+    }
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
         return [
           ...uriLocations,
-          `/images/tokens/${currency.symbol.toLowerCase() === 'wbnb' ? 'bnb' : currency.symbol.toLowerCase() === 'wftm' ? 'ftm' : 'token'}.png`,
           `/images/tokens/${currency?.address ?? 'token'}.png`,
           getTokenLogoURL(currency.address)
         ]
       }
       return [
-        `/images/tokens/${currency.symbol.toLowerCase() === 'wbnb' ? 'bnb' : currency.symbol.toLowerCase() === 'wftm' ? 'ftm' : 'token'}.png`,
         `/images/tokens/${currency?.address ?? 'token'}.png`,
         getTokenLogoURL(currency.address)]
     }
     return []
-  }, [config.baseCurrency, currency, uriLocations])
-
-  if (currency === config.baseCurrency) {
-    return (
-      <StyledBnbLogo src={`/images/tokens/${config.networkToken.symbol.toLowerCase()}.png`} size={size} style={style} />
-    )
-  }
+  }, [config.baseCurrency, currency, uriLocations, netWorkTokenIconImg])
 
   return <StyledLogo size={size} srcs={srcs} alt={`${currency?.symbol ?? 'token'} logo`} style={style} />
 }
--- src/views/Farms/components/FarmCard/CardHeading.tsx
+++ src/views/Farms/components/FarmCard/CardHeading.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Flex, Heading, Image } from 'uikit'
+import { useAllTokens } from 'hooks/Tokens'
+import { filterTokens } from '../filtering'
 
 export interface ExpandableSectionProps {
   lpLabel?: string
@@ -36,20 +38,28 @@ const IconImageSm = styled(Image)`
 `
 
 const CardHeading: React.FC<ExpandableSectionProps> = ({ lpLabel, farmImage, tokenSymbol }) => {
+  const allTokens = useAllTokens()
   const img_split = farmImage.split('-')
   const primaryImg = img_split[0]
   const secondaryImg = img_split[1]
+
+  const searchPrimaryTokenIconName = filterTokens(Object.values(allTokens), primaryImg)
+  const searchSecondTokenIconName = filterTokens(Object.values(allTokens), secondaryImg)
+
+  const primaryImgName = searchPrimaryTokenIconName[0]?.address
+  const secondaryImgName = searchSecondTokenIconName[0]?.address  
+
   return (
     <Wrapper flexDirection="column" justifyContent="space-between" alignItems="center" mb="12px">
       {img_split.length > 0 ? (
         <>
-          <Image src={`/images/tokens/${secondaryImg}.png`} alt={tokenSymbol} width={64} height={64} mb="6px" />
+          <Image src={`/images/tokens/${secondaryImgName}.png`} alt={tokenSymbol} width={64} height={64} mb="6px" />
           <div style={{ position: 'absolute', margin: '31px 0 0 35px', borderRadius: '10px' }}>
-            <IconImageSm src={`/images/tokens/${primaryImg}.png`} alt="icon" width={32} height={32} mr="3px" />
+            <IconImageSm src={`/images/tokens/${primaryImgName}.png`} alt="icon" width={32} height={32} mr="3px" />
           </div>
         </>
       ) : (
-        <Image src={`/images/farms/${farmImage}.svg`} alt={tokenSymbol} width={64} height={64} mb="6px" />
+        <Image src={`/images/tokens/${primaryImgName}.png`} alt={tokenSymbol} width={64} height={64} mb="6px" />
       )}
 
       <Flex justifyContent="center">
